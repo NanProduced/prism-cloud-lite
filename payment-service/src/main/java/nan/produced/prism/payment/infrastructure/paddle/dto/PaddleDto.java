9823a20dc8279c6b481118e2e13c77fcd685990c
Nan <nanproduced@gmail.com>
2026-04-20 16:37:58 +0800

    r3

--- payment-service/src/main/java/nan/produced/prism/payment/infrastructure/paddle/dto/PaddleDto.java
+++ payment-service/src/main/java/nan/produced/prism/payment/infrastructure/paddle/dto/PaddleDto.java
@@ -1,4 +1,4 @@
-package nan.produced.prism.payment.infrastructure.paddle;
+package nan.produced.prism.payment.infrastructure.paddle.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
